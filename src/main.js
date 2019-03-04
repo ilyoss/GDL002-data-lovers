dbb2e590e76613384e6bbcb511ac7c2b1cb72bf8
ilyoss <ily.trevino@live.com>
2019-03-03 21:37:14 -0600

    Proyecto terminado v2. Tests and better interface

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 //I save my news data as an array in my variable
 const news = window.STEAM.appnews.newsitems;
+document.getElementById("scroll").addEventListener("click", topFunction);
 //I declare my event listeners for my HTML elements
 document.getElementById("organizeByAuthor").addEventListener("click", () => {
   let organizedByTitleArray = window.dataLovers.displayNewsByTitle(news);
@@ -18,4 +19,20 @@ menu();
 function menu(){
   document.getElementById("root").innerHTML = `
   ${news.map(window.dataLovers.newsTemplate).join("")}`;
+}
+
+window.onscroll = function() {scrollFunction();};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scroll").style.display = "block";
+  } else {
+    document.getElementById("scroll").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
